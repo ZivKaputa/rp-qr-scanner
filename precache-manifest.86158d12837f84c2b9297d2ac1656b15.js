86f69e0ea18ffe13b4395b70ed3228416f594999
Ziv Kaputa <ziv.kaputa@gmail.com>
2019-08-27 00:35:01 -0500

    Updates

---
+++ precache-manifest.86158d12837f84c2b9297d2ac1656b15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f67431456dd693e71b94e2acf31de6f",
+    "url": "/rp-qr-scanner/index.html"
+  },
+  {
+    "revision": "66fd17003e0a6a27ed41",
+    "url": "/rp-qr-scanner/static/css/2.d58fcd6a.chunk.css"
+  },
+  {
+    "revision": "76febd61f087d179af36",
+    "url": "/rp-qr-scanner/static/css/main.f4930b89.chunk.css"
+  },
+  {
+    "revision": "66fd17003e0a6a27ed41",
+    "url": "/rp-qr-scanner/static/js/2.a4c87e31.chunk.js"
+  },
+  {
+    "revision": "76febd61f087d179af36",
+    "url": "/rp-qr-scanner/static/js/main.aada77e2.chunk.js"
+  },
+  {
+    "revision": "dd6095ee5ac72d5f5395",
+    "url": "/rp-qr-scanner/static/js/runtime~main.92519893.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rp-qr-scanner/static/media/logo.5d5d9eef.svg"
+  }
+]);
